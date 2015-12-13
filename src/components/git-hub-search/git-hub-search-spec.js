82f119a28fc6ffaa0576e3f47754b116ae573459
Mato Ilic <info@matoilic.ch>
2015-12-13 16:49:12 +0100

    search service tests

--- src/components/git-hub-search/git-hub-search-spec.js
+++ src/components/git-hub-search/git-hub-search-spec.js
@@ -2,10 +2,44 @@ import angular from 'angular';
 import 'angular-mocks';
 import component from './index';
 
-describe('Git Hub Search', () => {
-    beforeEach(angular.mock.module(component.name));
+describe('Git Hub Search service', () => {
+    let apiService;
+    let searchService;
 
-    it('should pass the dummy test to verify the karma setup', () => {
-        expect(true).toEqual(true);
+    beforeEach(() => {
+        angular.mock.module(component.name);
+
+        angular.mock.inject((gitHubApi, gitHubSearchService, $q) => {
+            apiService = gitHubApi;
+            searchService = gitHubSearchService;
+
+            spyOn(gitHubApi, 'listRepositories').and.returnValue({});
+            spyOn(gitHubApi, 'searchRepositories').and.returnValue({});
+        });
+    });
+
+    it('should list all public repositories if the search term is empty', () => {
+        searchService.find('');
+
+        expect(apiService.listRepositories).toHaveBeenCalled();
+        expect(apiService.searchRepositories).not.toHaveBeenCalled();
+    });
+
+    it('should search for repositories if the search term is not empty', () => {
+        const term = 'angular';
+
+        searchService.find(term);
+
+        expect(apiService.listRepositories).not.toHaveBeenCalled();
+        expect(apiService.searchRepositories).toHaveBeenCalledWith(term);
+    });
+
+    it('should cache search operations', () => {
+        const term = 'angular';
+
+        searchService.find(term);
+        searchService.find(term);
+
+        expect(apiService.searchRepositories.calls.count()).toBe(1);
     });
 });
