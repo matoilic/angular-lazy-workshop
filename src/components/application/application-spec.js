275724dd023b5681fcf58c92d18a7fb0e8cefb5a
Mato Ilic <info@matoilic.ch>
2015-12-13 16:16:41 +0100

    test if api service is configured properly

--- src/components/application/application-spec.js
+++ src/components/application/application-spec.js
@@ -2,10 +2,28 @@ import angular from 'angular';
 import 'angular-mocks';
 import applicationComponent from './index';
 
-describe('Application Component', function() {
-    beforeEach(angular.mock.module(applicationComponent.name));
+describe('Application Component', () => {
+    const apiUsername = 'john';
+    const apiToken = 'smith1234';
+    let apiProvider;
 
-    it('should pass the dummy test to verify the karma setup', function() {
-        expect(true).toEqual(true);
+    beforeEach(() => {
+        localStorage.setItem('git-hub-username', apiUsername);
+        localStorage.setItem('git-hub-token', apiToken);
+
+        angular.mock.module(applicationComponent.name, (gitHubApiProvider) => {
+            apiProvider = gitHubApiProvider;
+        });
+
+        angular.mock.inject((gitHubApi) => { });
+    });
+
+    afterEach(() => {
+        localStorage.clear();
+    });
+
+    it('should configure the GitHub API service', () => {
+        expect(apiProvider.username).toBe(apiUsername);
+        expect(apiProvider.token).toBe(apiToken);
     });
 });
